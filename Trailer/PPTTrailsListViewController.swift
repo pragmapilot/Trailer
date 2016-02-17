56082b4fc1a78e45918b66a20d25e5fc0fda5c36
Rodrigo Goncalves <rodrigo.goncalves@sherpany.com>
2016-02-17 14:56:36 +0000

    Refactors code to ease the future removal of scaffolding parts

--- Trailer/PPTTrailsListViewController.swift
+++ Trailer/PPTTrailsListViewController.swift
@@ -11,40 +11,41 @@ import CoreData
 
 class PPTTrailsListViewController: UITableViewController {
 
-    // TODO Refactor
-    var trails: Array<Trail>?
-    // TODO Refactor
-    var persistenceManager: PPTPersistenceManager?
+    private var trails: Array<Trail>?     // TODO Refactor
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // TODO Refactor
-        persistenceManager = PPTPersistenceManager()
-        let trail1 = NSEntityDescription.insertNewObjectForEntityForName("Trail", inManagedObjectContext: self.persistenceManager!.managedObjectContext) as! Trail
-        let trail2 = NSEntityDescription.insertNewObjectForEntityForName("Trail", inManagedObjectContext: self.persistenceManager!.managedObjectContext) as! Trail
-        
-        trail1.trail_description = "My first trail"
-        trail2.trail_description = "My second trail"
-     
-        self.trails = [trail1,trail2]
-        
-        // Don't show empty cells...
-        tableView.tableFooterView = UIView()
+
+        setData()           // TODO Remove
+        configureView()
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Return the count of the number of rows in the table
-        return (self.trails?.count)!
+        return (self.trails?.count)!     // TODO Refactor
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier", forIndexPath: indexPath) as UITableViewCell
-        // TODO Refactor: Move cell data binding
-        cell.textLabel?.text = self.trails![indexPath.row].trail_description
+
+        cell.textLabel?.text = self.trails![indexPath.row].trail_description         // TODO Refactor: Move cell data binding
         
         return cell
+    }
+    
+    private func configureView() {
+        // Show no empty cells
+        tableView.tableFooterView = UIView()
+    }
+    
+    private func setData(){
+        let persistenceManager = PPTPersistenceManager()
+        let trail1 = NSEntityDescription.insertNewObjectForEntityForName("Trail", inManagedObjectContext: persistenceManager.managedObjectContext) as! Trail
+        let trail2 = NSEntityDescription.insertNewObjectForEntityForName("Trail", inManagedObjectContext: persistenceManager.managedObjectContext) as! Trail
+        
+        trail1.trail_description = "My first trail"
+        trail2.trail_description = "My second trail"
+        
+        self.trails = [trail1,trail2]
     }
 }
 
